701d00c5db5b71508fb009fd7fba0a762c13bc26
Sleep-all-day <48984608+Sleep-all-day@users.noreply.github.com>
2021-09-12 22:02:55 +0800

    17

--- Leetcode/17电话号码的字母组合.py
+++ Leetcode/17电话号码的字母组合.py
@@ -4,15 +4,34 @@ class Solution(object):
             return 0
 
         phoneMap = {
-            2: "abc",
-            3: "def",
-            4: "ghi",
-            5: "jkl",
-            6: "mno",
-            7: "pqrs",
-            8: "tuv",
-            9: "xyz"
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
         }
 
+        def backtrack(index):
+            if index == len(digits):
+                combinations.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phoneMap[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
 
+        combination = list()
+        combinations = list()
+        backtrack(0)
 
+        return combinations
+
+if __name__ =="__main__":
+    s = Solution()
+    digits = "23"
+    ans = s.letterCombinations(digits)
+    print(ans)
